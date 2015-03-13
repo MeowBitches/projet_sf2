3cdaad757eedbab8f95814f6147c9cd05b2576fe
thishirt <ng.thi90@gmail.com>
2015-03-13 12:09:27 +0100

    footer

--- web/_assets/js/menu-profil.js
+++ web/_assets/js/menu-profil.js
@@ -1,19 +1,7 @@
 $(function() {
 	$('.image-profil').click(function() {
-
-	 if ($('.boite-menu').hasClass("hidden") ) {
-			$('.boite-menu').removeClass("hidden");
-			$('.boite-menu').addClass("showed");	
-			/*$('.boite-menu').show();*/
-	 		}
-	 else {
-		$('.boite-menu').removeClass("showed");	
-		$('.boite-menu').addClass("hidden");		
-		/*$('.boite-menu').hide();*/
-	 	}
-
+		$('.boite-menu').fadeToggle(100);
 	});
-
 });
 
 
